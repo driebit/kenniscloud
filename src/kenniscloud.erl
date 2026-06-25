dd0d00c6c98fc77a8a2c80c0128b3a5b02c7294c
Rob van den Bogaard <rob@driebit.nl>
2026-06-25 15:09:32 +0200

    KCI-70: Add existing group members to parent when added as subgroup

--- src/kenniscloud.erl
+++ src/kenniscloud.erl
@@ -701,6 +701,13 @@ observe_edge_insert(#edge_insert{predicate=has_subgroup, subject_id = ParentId, 
         end,
         m_edge:objects(ParentId, hascollabmanager, Context)
     ),
+    % add any existing users from the subgroup, also to the parent group (like how it is done on join)
+    lists:foreach(
+        fun (MemberId) ->
+            m_edge:insert(ParentId, hascollabmember, MemberId, Context)
+        end,
+        m_edge:objects(SubGroupId, hascollabmember, Context)
+    ),
     ok;
 observe_edge_insert(#edge_insert{predicate=like, subject_id=SubjectId, object_id=ObjectId}, Context) ->
     case m_rsc:is_a(SubjectId, person, Context) of
